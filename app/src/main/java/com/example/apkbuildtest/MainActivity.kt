12bdbb0ebbdd6e9f3743542685dc993d8e679438
WhimsyWretchWoods <riko.e610@gmail.com>
2025-06-18 18:04:22 +0700

    Update MainActivity.kt

--- app/src/main/java/com/example/apkbuildtest/MainActivity.kt
+++ app/src/main/java/com/example/apkbuildtest/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.apkbuildtest
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        
+        setContent {
+            Greeting("APK Build Test")
+        }
     }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
 }
