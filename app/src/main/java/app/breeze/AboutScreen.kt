96835cc5e5e31b771dd56781005fcace3929c9c9
WhimsyWretchWoods <riko.e610@gmail.com>
2025-07-04 15:57:02 +0700

    Create AboutScreen.kt

---
+++ app/src/main/java/app/breeze/AboutScreen.kt
@@ -0,0 +1,25 @@
+package app.breeze
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun AboutScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "I etch my failures into the marrow of my being, while my triumphs fade like whispers in a storm.", style = MaterialTheme.typography.headlineMedium)
+    }
+}
