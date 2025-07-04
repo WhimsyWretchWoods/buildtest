4ae219184c84c705fcd101cd645e196610fa9609
WhimsyWretchWoods <riko.e610@gmail.com>
2025-07-04 15:57:39 +0700

    Create AppRoutes.kt

---
+++ app/src/main/java/app/breeze/AppRoutes.kt
@@ -0,0 +1,8 @@
+package app.breeze
+
+object AppRoutes {
+    const val FOLDER_SCREEN = "folder_screen"
+    const val IMAGE_SCREEN = "image_screen/{folderPath}"
+    const val FULL_SCREEN = "full_screen/{imageUri}"
+    const val ABOUT_SCREEN = "about_screen"
+}
