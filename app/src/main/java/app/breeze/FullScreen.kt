39651d5b3d1dbc6f7173bd8108949652dd998ce4
WhimsyWretchWoods <riko.e610@gmail.com>
2025-07-04 16:18:51 +0700

    Update FullScreen.kt

--- app/src/main/java/app/breeze/FullScreen.kt
+++ app/src/main/java/app/breeze/FullScreen.kt
@@ -5,8 +5,10 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.toSize
 import coil.compose.rememberAsyncImagePainter
 import net.engawapg.lib.zoomable.rememberZoomState
 import net.engawapg.lib.zoomable.zoomable
@@ -15,7 +17,7 @@ import net.engawapg.lib.zoomable.zoomable
 fun FullScreen(imageUri: String) {
     val painter = rememberAsyncImagePainter(model = Uri.parse(imageUri))
     val zoomState = rememberZoomState(
-        contentSize = painter.intrinsicSize
+        contentSize = painter.intrinsicSize.takeIf { it.width > 0f && it.height > 0f } ?: remember { androidx.compose.ui.geometry.Size.Zero }
     )
 
     Box(modifier = Modifier.fillMaxSize()) {
