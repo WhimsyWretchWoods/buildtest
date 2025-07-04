e45ae813ebfc888a7870262168f3f727cb6cb316
WhimsyWretchWoods <riko.e610@gmail.com>
2025-07-04 15:20:22 +0700

    Create MainActivity.kt

---
+++ app/src/main/java/app/test/MainActivity.kt
@@ -0,0 +1,21 @@
+package app.test
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Greeting("APK Build Test")
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
