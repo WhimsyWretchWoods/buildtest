16e88c4fe056167cb6c8b766aa571fd7eb1d0915
WhimsyWretchWoods <riko.e610@gmail.com>
2025-07-05 16:52:43 +0700

    Update Dialogs.kt

--- app/src/main/java/app/breeze/ui/components/Dialogs.kt
+++ app/src/main/java/app/breeze/ui/components/Dialogs.kt
@@ -5,8 +5,8 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.foundation.layout.Column
-
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 @Composable
 fun ConfirmDeleteDialog(
     showDialog: Boolean,
@@ -47,10 +47,9 @@ fun InfoDialog(
             onDismissRequest = onDismiss,
             title = { Text(title) },
             text = {
-                Column(modifier = modifier) {
-                    Text("Selected Items:")
-                    infoItems.forEach { item ->
-                        Text(" - $item")
+                LazyColumn() {
+                    items(infoItems) { item ->
+                        Text(item)
                     }
                 }
             },
