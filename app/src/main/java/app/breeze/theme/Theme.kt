f89c805adceaf4be34f208d9d44f97832c94c5ee
WhimsyWretchWoods <riko.e610@gmail.com>
2025-07-05 21:27:24 +0700

    Update Theme.kt

--- app/src/main/java/app/breeze/theme/Theme.kt
+++ app/src/main/java/app/breeze/theme/Theme.kt
@@ -1,22 +1,30 @@
 package app.breeze.theme
 
+import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme()
-
 private val LightColorScheme = lightColorScheme()
 
 @Composable
 fun Breeze(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable () -> Unit) {
-    val colorScheme = when {
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+    content: @Composable () -> Unit
+) {
+    val context = LocalContext.current
+    val colorScheme = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+        if (darkTheme) dynamicDarkColorScheme(context)
+        else dynamicLightColorScheme(context)
+    } else {
+        if (darkTheme) DarkColorScheme
+        else LightColorScheme
     }
 
     MaterialTheme(
