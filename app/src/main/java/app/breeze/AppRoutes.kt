9d7cf05025101a194057fa08bfd2b5082ae0990c
WhimsyWretchWoods <riko.e610@gmail.com>
2025-07-05 18:19:37 +0700

    Update AppRoutes.kt

--- app/src/main/java/app/breeze/AppRoutes.kt
+++ app/src/main/java/app/breeze/AppRoutes.kt
@@ -2,7 +2,7 @@ package app.breeze
 
 object AppRoutes {
     const val FOLDER_SCREEN = "folder_screen"
-    const val IMAGE_SCREEN = "image_screen/{folderPath}"
+    const val IMAGE_SCREEN = "image_screen/{folderPath}/{folderName}"
     const val FULL_SCREEN = "full_screen/{imageUri}"
     const val ABOUT_SCREEN = "about_screen"
 }
