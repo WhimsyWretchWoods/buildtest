99d9b051651b6508ef7261fc0aa5a722815ea169
WhimsyWretchWoods <riko.e610@gmail.com>
2025-07-05 15:20:31 +0700

    Update Dialogs.kt

--- app/src/main/java/app/breeze/ui/components/Dialogs.kt
+++ app/src/main/java/app/breeze/ui/components/Dialogs.kt
@@ -5,6 +5,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.foundation.layout.Column
 
 @Composable
 fun ConfirmDeleteDialog(
