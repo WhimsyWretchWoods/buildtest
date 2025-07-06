91c3558b46b2f3815f39071eb805df89e5c3bf94
WhimsyWretchWoods <riko.e610@gmail.com>
2025-07-06 15:55:04 +0700

    Update ImageScreen.kt

--- app/src/main/java/app/breeze/ImageScreen.kt
+++ app/src/main/java/app/breeze/ImageScreen.kt
@@ -29,7 +29,7 @@ import androidx.compose.ui.input.nestedscroll.nestedScroll
 
 import app.breeze.data.ImageFetcher
 import app.breeze.data.ImageDetails
-import app.breeze.data.ImageDetailsFetcher
+import app.breeze.data.getDetails
 import app.breeze.ui.components.ConfirmDeleteDialog
 import app.breeze.ui.components.InfoDialog
 import app.breeze.ui.components.SelectionFloatingToolbar
@@ -95,7 +95,7 @@ fun ImageScreen(navController: NavController, folderPath: String, folderName: St
                     onInfoClick = { 
                         coroutineScope.launch {
                             imageDetailsToShow = selectedImageUris.mapNotNull { uriString ->
-                                ImageDetailsFetcher.getDetails(context, Uri.parse(uriString))
+                                ImageFetcher.getDetails(context, Uri.parse(uriString))
                             }
                             showInfoDialog = true
                         }
