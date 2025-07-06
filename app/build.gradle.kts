59aa785c5f690b08448a9f413b446a8bdfc76148
WhimsyWretchWoods <riko.e610@gmail.com>
2025-07-06 11:24:52 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,7 +60,7 @@ android {
 }
 
 dependencies {
-    implementation("androidx.compose.ui:ui:1.8.3")
+    implementation("androidx.compose.ui:ui:1.9.0-beta02")
     implementation("androidx.compose.material3:material3:1.4.0-alpha17")
     implementation("androidx.activity:activity-compose:1.10.1")
     implementation("androidx.compose.material:material-icons-extended:1.7.8")
