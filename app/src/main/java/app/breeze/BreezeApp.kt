bc93a5097bfada7e94ad3c0d96bda9058da7b1a8
WhimsyWretchWoods <riko.e610@gmail.com>
2025-07-06 15:01:40 +0700

    Update and rename MainApp.kt to BreezeApp.kt

---
+++ app/src/main/java/app/breeze/BreezeApp.kt
@@ -0,0 +1,115 @@
+package app.breeze
+
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.NavType
+import androidx.navigation.navArgument
+import app.breeze.data.ThemePreferenceRepository
+
+@Composable
+fun BreezeApp(themeRepository: ThemePreferenceRepository) {
+    val navController = rememberNavController()
+
+    Surface(modifier = Modifier.fillMaxSize()) {
+        NavHost(
+            navController = navController,
+            startDestination = AppRoutes.FOLDER_SCREEN,
+            enterTransition = {
+                slideInHorizontally(
+                    initialOffsetX = { fullWidth -> fullWidth },
+                    animationSpec = spring(
+                        dampingRatio = Spring.DampingRatioLowBouncy,
+                        stiffness = Spring.StiffnessMedium
+                    )
+                ) + fadeIn(
+                    animationSpec = spring(
+                        dampingRatio = Spring.DampingRatioNoBouncy,
+                        stiffness = Spring.StiffnessMedium
+                    )
+                )
+            },
+            exitTransition = {
+                slideOutHorizontally(
+                    targetOffsetX = { fullWidth -> -fullWidth },
+                    animationSpec = spring(
+                        dampingRatio = Spring.DampingRatioLowBouncy,
+                        stiffness = Spring.StiffnessMedium
+                    )
+                ) + fadeOut(
+                    animationSpec = spring(
+                        dampingRatio = Spring.DampingRatioNoBouncy,
+                        stiffness = Spring.StiffnessMedium
+                    )
+                )
+            },
+            popEnterTransition = {
+                slideInHorizontally(
+                    initialOffsetX = { fullWidth -> -fullWidth },
+                    animationSpec = spring(
+                        dampingRatio = Spring.DampingRatioLowBouncy,
+                        stiffness = Spring.StiffnessMedium
+                    )
+                ) + fadeIn(
+                    animationSpec = spring(
+                        dampingRatio = Spring.DampingRatioNoBouncy,
+                        stiffness = Spring.StiffnessMedium
+                    )
+                )
+            },
+            popExitTransition = {
+                slideOutHorizontally(
+                    targetOffsetX = { fullWidth -> fullWidth },
+                    animationSpec = spring(
+                        dampingRatio = Spring.DampingRatioLowBouncy,
+                        stiffness = Spring.StiffnessMedium
+                    )
+                ) + fadeOut(
+                    animationSpec = spring(
+                        dampingRatio = Spring.DampingRatioNoBouncy,
+                        stiffness = Spring.StiffnessMedium
+                    )
+                )
+            }
+        ) {
+            composable(AppRoutes.FOLDER_SCREEN) {
+                FolderScreen(navController = navController)
+            }
+            composable(
+                route = AppRoutes.IMAGE_SCREEN,
+                arguments = listOf(
+                    navArgument("folderPath") {type = NavType.StringType},
+                    navArgument("folderName") {type = NavType.StringType}
+                )
+            ) { backStackEntry ->
+                val folderPath = backStackEntry.arguments?.getString("folderPath") ?: "null"
+                val folderName = backStackEntry.arguments?.getString("folderName") ?: "null"
+                ImageScreen(navController = navController, folderPath, folderName)
+            }
+            composable(
+                route = AppRoutes.FULL_SCREEN,
+                arguments = listOf(navArgument("imageUri") {
+                    type = NavType.StringType
+                })
+            ) { backStackEntry ->
+                val imageUri = backStackEntry.arguments?.getString("imageUri") ?: ""
+                FullScreen(imageUri, navController)
+            }
+            composable(AppRoutes.SETTINGS_SCREEN) {
+                SettingsScreen(navController, themeRepository)
+            }
+        }
+    }
+}
