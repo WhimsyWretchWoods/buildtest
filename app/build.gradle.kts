f583050542753a396846af1d61be7f3b76694750
WhimsyWretchWoods <riko.e610@gmail.com>
2025-07-06 12:49:01 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,4 +67,5 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.9.0")
     implementation("io.coil-kt:coil-compose:2.7.0")
     implementation("net.engawapg.lib:zoomable:2.8.0")
+    implementation("androidx.datastore:datastore-preferences:1.1.7")
 }
