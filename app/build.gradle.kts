c4dd5c41d290c289106fe5f86ff89fafb4dfbb0b
WhimsyWretchWoods <riko.e610@gmail.com>
2025-07-12 16:22:30 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,7 +62,7 @@ android {
 
 dependencies {
     implementation("androidx.compose.ui:ui:1.9.0-beta02")
-    implementation("androidx.compose.material3:material3:1.3.2")
+    implementation("androidx.compose.material3:material3:1.4.0-alpha17")
     implementation("com.google.android.material:material:1.14.0-alpha02")
     implementation("androidx.activity:activity-compose:1.10.1")
     implementation("androidx.navigation:navigation-compose:2.9.0")
