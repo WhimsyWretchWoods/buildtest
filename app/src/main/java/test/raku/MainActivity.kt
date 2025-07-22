fc0c0b3747225f7d8863628907114812ec3fa6dd
WhimsyWretchWoods <riko.e610@gmail.com>
2025-07-22 22:50:41 +0700

    Update MainActivity.kt

--- app/src/main/java/test/raku/MainActivity.kt
+++ app/src/main/java/test/raku/MainActivity.kt
@@ -5,13 +5,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.activity.compose.setContent
 import android.os.Bundle
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import androidx.compose.material3.Surface
+
 class MainActivity : ComponentActivity() {
 
-	override fun onCreate(savedInstanceState: Bundle?) {
-		super.onCreate(savedInstanceState)
-		enableEdgeToEdge()
-		setContent {
-			Player()
-		}
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AppTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    Player()
+                }
+            }
+        }
     }
 }
